e31157d91ce114b555c6f12daf41f47cb541725c
Atest <3285923@gmail.com>
2021-03-31 17:42:13 +0500

    update_homework

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -1,6 +1,5 @@
 import pytest
 from selenium.webdriver.common.by import By
-import time
 
 
 @pytest.mark.nondestructive
@@ -9,7 +8,6 @@ def test_product_page(browser, base_url):
     url = "index.php?route=product/product&path=57&product_id=49"
     browser.get(f'{base_url}' + f'{url}')
     item_title = "Samsung Galaxy Tab 10.1"
-    time.sleep(1)
     assert browser.find_elements(By.XPATH, "//a[@class='thumbnail']")[0].get_attribute("title") == item_title
     assert browser.find_element(By.XPATH, "//div[@class='col-sm-4']/h1").text == item_title
     assert browser.find_element(By.ID, "button-cart").text == "Add to Cart"
